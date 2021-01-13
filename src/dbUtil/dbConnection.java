f2f45995c8d6be4cef067733e12b36a410c3b9f1
yovana13 <73619767+yovana13@users.noreply.github.com>
2021-01-13 01:13:13 +0100

    Update dbConnection.java

--- src/dbUtil/dbConnection.java
+++ src/dbUtil/dbConnection.java
@@ -2,19 +2,21 @@ package dbUtil;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class dbConnection {
-    public static Connection getConnection() throws Exception{
+    public static Connection getConnection() throws SQLException {
         try {
             String url = "jdbc:mysql://localhost:3306/demo";
             String username = "root";
             String password = "matematika269";
 
             Connection conn = DriverManager.getConnection(url, username, password);
-            // System.out.println("Connected.");
+
             return conn;
-        }catch (Exception e){
-            System.out.println(e);
+        }catch (SQLException e){
+            System.out.println("SQL Exception found in the code");
+            //System.out.println(e);
         }
         return null;
     }
